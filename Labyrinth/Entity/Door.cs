048d07131857ef81a02a4a60779db35e2e62f823
Vladimir Melnik <vova@programmer.net>
2015-05-10 13:51:53 +0300

    add Maze

---
+++ Labyrinth/Entity/Door.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Labyrinth.Entity
+{
+    public class Door : MapSite
+    {
+        private readonly Room _roomFrst;
+        private readonly Room _roomScnd;
+        private bool _isOpen;
+
+        public Door(Room room1, Room room2)
+        {
+            _roomFrst = room1;
+            _roomScnd = room2;
+        }
+
+        public override void Enter()
+        {
+            Console.WriteLine("Door");
+        }
+
+        public Room OtherSide(Room room)
+        {
+            return room == _roomFrst ? _roomScnd : _roomFrst;
+        }
+    }
+}
---
+++ Labyrinth/Entity/Maze.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Labyrinth.Entity
+{
+    public class Maze
+    {
+        private Dictionary<int, Room> _rooms;
+
+        public Maze()
+        {
+            _rooms = new Dictionary<int, Room>();
+        }
+
+        public void AddRoom(Room room)
+        {
+            _rooms.Add(room.RoomNumber, room);
+        }
+
+        public Room RoomNo(int number)
+        {
+            return _rooms[number];
+        }
+    }
+}
