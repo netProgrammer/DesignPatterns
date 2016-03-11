9105e518ef48f068e85d77b20c138851c1002d79
Melnyk Vladimir <melnik@dev.expoglobus.com>
2016-03-11 21:29:06 +0200

    NullObject
    NullObject

---
+++ NullObject/NullObject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NullObject
+{
+    class Program
+    {
+        static void Main()
+        {
+            ISomeObject realObject = new SomeObject();
+            realObject.DoSomething();
+
+            ISomeObject nullObject = new NullObject();
+            nullObject.DoSomething();
+
+            Console.ReadLine();
+        }
+    }
+
+    interface ISomeObject
+    {
+        void DoSomething();
+    }
+
+    class NullObject : ISomeObject
+    {
+        public void DoSomething()
+        {
+            //Nothing
+        }
+    }
+
+    class SomeObject : ISomeObject
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Just do It !");
+        }
+    }
+}
