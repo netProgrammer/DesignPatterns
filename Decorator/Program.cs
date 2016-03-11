0e665eda70b894e647f7da09a7b223ef78b0867f
Melnyk Vladimir <melnik@dev.expoglobus.com>
2016-03-11 21:54:18 +0200

    Decorator

---
+++ Decorator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Component component = new Component();
+            DecoratorImplA decoratorA = new DecoratorImplA();
+            DecoratorImplB decoratorB = new DecoratorImplB();
+
+            decoratorA.SetComponent(component);
+            decoratorB.SetComponent(decoratorA);
+
+            decoratorA.DoSomething();
+            decoratorB.DoSomething();
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IComponent
+    {
+        void DoSomething();
+    }
+
+    class Component : IComponent
+    {
+        public virtual void DoSomething()
+        {
+           Console.WriteLine("Component");    
+        }
+    }
+
+    abstract class Decorator : Component
+    {
+        private Component _component;
+
+        public void SetComponent(Component component)
+        {
+            _component = component;
+        }
+
+        public override void DoSomething()
+        {
+            if (_component != null)
+            {
+                _component.DoSomething();
+            }
+        }
+    }
+
+    class DecoratorImplA : Decorator
+    {
+        
+    }
+
+    class DecoratorImplB : Decorator
+    {
+        public override void DoSomething()
+        {
+            base.DoSomething();
+            Console.WriteLine("DecoratorImplB");
+        }
+    }
+}
