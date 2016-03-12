5c8465c3abb89a791d010eb2a9e38475387bb0e3
Melnyk Vladimir <melnik@dev.expoglobus.com>
2016-03-12 23:16:54 +0200

    HelloDI

---
+++ HelloDI/HelloDI/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HelloDI
+{
+    class Program
+    {
+        static void Main()
+        {
+            IMessageWriter writer = new ConsoleMessageWriter();
+            var salutation = new Salutation(writer);
+            salutation.Exclaim();
+        }
+    }
+
+    internal class Salutation
+    {
+        private readonly IMessageWriter _messageWriter;
+        public Salutation(IMessageWriter messageWriter)
+        {
+            _messageWriter = messageWriter;
+        }
+
+        public void Exclaim()
+        {
+            _messageWriter.Write("Hello DI!");
+        }
+    }
+
+    internal class ConsoleMessageWriter : IMessageWriter
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+
+    internal interface IMessageWriter
+    {
+        void Write(string message);
+    }
+}
