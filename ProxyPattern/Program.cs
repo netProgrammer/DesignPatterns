624318f8a76f7f62c1feff9af53780b059877076
melnik <melnik@dev.expoglobus.com>
2018-07-14 22:30:56 +0300

    Proxy Pattern

---
+++ ProxyPattern/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ProxyPattern
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***Proxy Pattern Demo***\n");
+            ISubject px = new Proxy();
+            px.DoSomeWork();
+            Console.ReadKey();
+        }
+    }
+
+    internal class Proxy: ISubject
+    {
+        private ISubject _subject;
+        public void DoSomeWork()
+        {
+            Console.WriteLine("Proxy call happening");
+            if (_subject == null)
+            {
+                _subject = new ConcreteSubject();                
+            }
+            
+            _subject.DoSomeWork();
+        }
+    }
+
+    interface ISubject
+    {
+        void DoSomeWork();
+    } 
+    
+    internal class ConcreteSubject: ISubject
+    {
+        public void DoSomeWork()
+        {
+            Console.WriteLine("ConcreteSubject.DoSomeWork");
+        }
+    }
+}
